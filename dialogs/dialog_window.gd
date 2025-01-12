8daed76aa190546883c3d66e2e179117d30014ec
Nick Kurkurin <nlk@reshape.team>
2025-01-12 19:54:21 +0100

    master
    1) Add new keybind for activate dialog;
    2) Align dialog in the bottom of screen;

--- dialogs/dialog_window.gd
+++ dialogs/dialog_window.gd
@@ -4,9 +4,10 @@ extends VBoxContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	TranslationServer.set_locale("ru")
+	$ReplicaOptions/ReplicaOption1.set_text(tr("test1"))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$GridContainer/Button.set_text(tr("test1"))
+	pass
